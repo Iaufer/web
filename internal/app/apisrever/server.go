098dfd4312c36b9e233108952ca8e89a52cade82
elijahleep <68677740+elijahleep@users.noreply.github.com>
2024-11-29 17:08:03 +0700

    Update server.go private

--- internal/app/apisrever/server.go
+++ internal/app/apisrever/server.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"text/template"
 	"time"
@@ -75,34 +74,6 @@ func (s *server) configureRouter() {
 	private.Use(s.authenticateUser)
 	private.HandleFunc("/whoami", s.handleWhoami()).Methods("GET")
 	private.HandleFunc("/profile", s.handleProfile()).Methods("GET", "POST")
-	private.HandleFunc("/roles", s.getRoles).Methods("GET")
-
-}
-
-func (s *server) getRoles(w http.ResponseWriter, r *http.Request) {
-	roles, err := s.enforcer.GetAllRoles()
-
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	users, err := s.enforcer.GetAllSubjects()
-
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	data := map[string]interface{}{
-		"roles": roles,
-		"users": users,
-	}
-
-	println(data)
-
-	// s.respond(w, r, http.StatusOK, data) // здесь как то сделать так чтобы были все роли и юзеры их
-
-	s.respond(w, r, http.StatusOK, roles)
 
 }
 
@@ -119,19 +90,6 @@ func (s *server) handleProfile() http.HandlerFunc {
 			s.renderProfilePage(w, r, user)
 		case http.MethodPost:
 			// Если разрешение есть, создаем топик
-
-			allowed, err := s.enforcer.Enforce(user.Email, "topic", "create")
-
-			if err != nil {
-				s.error(w, r, http.StatusInternalServerError, err)
-				return
-			}
-
-			if !allowed {
-				s.error(w, r, http.StatusForbidden, errors.New("permission denied"))
-				return
-			}
-
 			s.createTopic(w, r, user)
 		default:
 			s.error(w, r, http.StatusMethodNotAllowed, errors.New("method not allowed"))
@@ -152,7 +110,7 @@ func (s *server) renderProfilePage(w http.ResponseWriter, r *http.Request, user 
 }
 
 func (s *server) createTopic(w http.ResponseWriter, r *http.Request, user *model.User) {
-	m, err := utils.ParseFormFields(r, []string{"topicname", "topicdescription", "isprivate", "topicabout", "topiccategory"})
+	m, err := utils.ParseFormFields(r, []string{"topicname", "topicdescription", "topicabout"})
 	if err != nil {
 		s.error(w, r, http.StatusBadRequest, err)
 		return
@@ -163,12 +121,17 @@ func (s *server) createTopic(w http.ResponseWriter, r *http.Request, user *model
 		return
 	}
 
+	isPrivate := false
+	if m["isprivate"] == "on" {
+		isPrivate = true
+	}
+
 	topic := &model.Topic{
 		UserID:      user.ID,
 		TopicName:   m["topicname"],
-		Description: m["topiccategory"], //в описании будут категории лень менять везде
-		Visibility:  m["isprivate"] == "on",
-		Content:     m["topicabout"], // здесь основной контент топика
+		Description: m["topicdescription"],
+		Visibility:  isPrivate,
+		Content:     m["topicabout"],
 	}
 
 	fmt.Println(*topic)
@@ -294,10 +257,12 @@ func (s *server) handleUsersCreate() http.HandlerFunc {
 			u.Sanitaze() // ответ без пароля юзера
 			// s.respond(w, r, http.StatusCreated, u) // тут сделать перенапрвление на вход
 
-			if err := addRoleForUser(u.Email, s.enforcer); err != nil {
-				s.error(w, r, http.StatusInternalServerError, err)
-				return
-			}
+			// err := addRoleForUser(u.Email, s.enforcer)
+
+			// if err != nil {
+			// 	log.Fatal(err)
+			// 	return
+			// }
 
 			http.Redirect(w, r, "/sessions", http.StatusSeeOther)
 		}
@@ -307,7 +272,13 @@ func (s *server) handleUsersCreate() http.HandlerFunc {
 func addRoleForUser(name string, e *casbin.Enforcer) error {
 	_, err := e.AddRoleForUser(name, "editor")
 
-	return err
+	if err != nil {
+		fmt.Println("Error in addRoleForUser")
+
+		return err
+	}
+
+	return nil
 }
 
 func (s *server) handleSessionsCreate() http.HandlerFunc {
